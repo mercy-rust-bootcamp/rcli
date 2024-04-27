d062cdc3b032f81cf17f0bd068afd9a856659e8d
Mercy Wang <wanghongxuan36@gmail.com>
2024-04-28 00:48:20 +0800

    feature: support zxcvbn to test password strength

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
 
 const UPPER: &[u8] = b"ABCDEFGHJKLMNPQRSTUVWXYZ";
 const LOWER: &[u8] = b"abcdefghijkmnopqrstuvwxyz";
@@ -42,7 +43,12 @@ pub fn process_genpass(
 
     password.shuffle(&mut rng);
 
-    println!("{}", String::from_utf8(password)?);
+    let password = String::from_utf8(password)?;
+    println!("{}", password);
+
+    // output password strength in stderr
+    let estimate = zxcvbn(&password, &[])?;
+    eprintln!("Password strength: {}", estimate.score());
 
     Ok(())
 }
