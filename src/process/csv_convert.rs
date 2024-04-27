1d681c4c1eedcc7ff4dd79c84d9aae850d9fb6a6
Mercy Wang <wanghongxuan36@gmail.com>
2024-04-28 00:41:21 +0800

    feature: add password generator

--- src/process/csv_convert.rs
+++ src/process/csv_convert.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -18,7 +17,7 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
     let headers = reader.headers()?.clone();
